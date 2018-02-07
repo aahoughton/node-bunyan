de8999b60cf8dee6ec43ce97e134a34dcfc6785c
JW Tsu <jtsu@electricimp.com>
2018-02-07 14:06:39 -0800

    Add index.d.ts file

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for bunyan 1.5.1
+// Project: https://github.com/aahoughton/node-bunyan
+// Definitions by: JW Tsu <https://github.com/jdubjdub>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'bunyan';
